51133ce6dfdbe7172b2bcb7755c7ef4689fae712
Max <max@vmc.ai>
2019-06-20 15:03:33 +0200

    Updates

---
+++ precache-manifest.cb9893f9e3a68ed38242e879f4e001c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429b588b9d24bf54533c532410cfba48",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "b954ce174e5d9a6b497e",
+    "url": "/website/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ebc4f64235d46c28e235",
+    "url": "/website/static/css/main.44bce43a.chunk.css"
+  },
+  {
+    "revision": "b954ce174e5d9a6b497e",
+    "url": "/website/static/js/2.fd431216.chunk.js"
+  },
+  {
+    "revision": "ebc4f64235d46c28e235",
+    "url": "/website/static/js/main.0d4a40fa.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  }
+]);
